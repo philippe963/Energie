2d6d9230752f10f281e88a861aa9889d95d21b31
philippe963 <161497063+philippe963@users.noreply.github.com>
2024-07-01 14:52:19 +0200

    Add files via upload

--- page1_garde.py
+++ page1_garde.py
@@ -2,7 +2,35 @@ import streamlit as st
 from PIL import Image
 
 def app():
-    st.title("Page de Garde")
-    st.write("Bienvenue dans le Projet Énergie")
+    st.markdown(
+        """
+        <style>
+        body {
+            margin: 0;
+            padding: 0;
+        }
+        .centered-title {
+            text-align: center;
+            font-size: 24px;  /* Vous pouvez ajuster la taille de la police si nécessaire */
+            margin: 0;
+        }
+        .image-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    # Afficher l'image en haut de la page
     image = Image.open("image/image-accueil2.jpg")  # Remplacez par le chemin de votre image
-    st.image(image, use_column_width=True)
+    st.markdown(
+        f'<div class="image-container"><img src="data:image/jpeg;base64,{st.image(image, use_column_width=True).data}" alt="Image" style="width: 100%;"></div>',
+        unsafe_allow_html=True
+    )
+    # Utilisation de HTML et CSS pour centrer le titre
+    st.markdown('<div class="centered-title">Bienvenue dans le Projet Énergie</div>', unsafe_allow_html=True)
+
+if __name__ == "__main__":
+    app()
