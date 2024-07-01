bde0582840ea4b3db3078f0abc44acb97d9af5e3
philippe963 <161497063+philippe963@users.noreply.github.com>
2024-07-01 16:31:19 +0200

    Add files via upload

--- page1_garde.py
+++ page1_garde.py
@@ -2,35 +2,21 @@ import streamlit as st
 from PIL import Image
 
 def app():
-    # Utilisation de CSS pour enlever les marges et paddings par défaut du body et centrer le titre
+    # Afficher l'image en haut de la page
+    image = Image.open("image/page-accueil.jpg")  # Remplacez par le chemin de votre image
+    st.image(image, use_column_width=True)
+    
+    # Utiliser la classe container pour structurer le contenu
     st.markdown(
         """
-        <style>
-        body {
-            margin: 0;
-            padding: 0;
-        }
-        .image-container {
-            text-align: center;
-            margin: 0;
-            padding: 0;
-        }
-        .centered-title {
-            text-align: center;
-            font-size: 24px;  /* Ajustez la taille de la police si nécessaire */
-            margin-top: 0;
-        }
-        </style>
+        <div class="container">
+            <h1>Bienvenue dans le Projet Énergie</h1>
+            <p>Dans un contexte mondial où la transition énergétique est devenue une priorité, la France se positionne en tant qu'acteur engagé dans la recherche d'un équilibre optimal entre la production d'énergie et les impératifs environnementaux.</p>
+            <p>Ce projet vise à prédire la consommation d'énergie en utilisant des techniques avancées de machine learning pour optimiser la production et répondre aux besoins de manière efficace et durable.</p>
+        </div>
         """,
         unsafe_allow_html=True
     )
-    
-    # Afficher l'image en haut de la page
-    image = Image.open("image/image-accueil2.jpg")  # Remplacez par le chemin de votre image
-    st.markdown(f'<div class="image-container"><img src="data:image/jpeg;base64,{st.image(image, use_column_width=True).data}" alt="Image" style="width: 100%; margin: 0; padding: 0;"></div>', unsafe_allow_html=True)
-    
-    # Utilisation de HTML et CSS pour centrer le titre
-    st.markdown('<div class="centered-title">Bienvenue dans le Projet Énergie</div>', unsafe_allow_html=True)
 
 if __name__ == "__main__":
     app()
